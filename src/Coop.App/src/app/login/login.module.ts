b736bd0fe3362140abb8e8b2d229054ff34a0dc7
QuinntyneBrown <quinntynebrown@gmail.com>
2021-08-16 00:45:39 -0400

    feat(login): create account

--- src/Coop.App/src/app/login/login.module.ts
+++ src/Coop.App/src/app/login/login.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { LoginFormModule } from './login-form/login-form.module';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     CommonModule,
