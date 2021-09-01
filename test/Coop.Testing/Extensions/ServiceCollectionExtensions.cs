dff917fc343c378378646b1f2bec9c12c0898eb5
QuinntyneBrown <quinntynebrown@gmail.com>
2021-09-01 19:56:51 -0400

    feat(test)

---
+++ test/Coop.Testing/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Coop.Api.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Coop.Testing
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInMemoryDbContext(this IServiceCollection services)
+        {
+            services.AddSingleton<ICoopDbContext>(new CoopDbContextBuilder()
+                .UseInMemoryDatabase()
+                .Build());
+
+            return services;
+        }
+    }
+}
--- test/Coop.UnitTests/Features/GetBoardMembersTests.cs
+++ test/Coop.UnitTests/Features/GetBoardMembersTests.cs
@@ -1,5 +1,4 @@
 ﻿using Coop.Api.Interfaces;
-using Coop.Api.Models;
 using Coop.Testing;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,37 +11,51 @@ namespace Coop.UnitTests.Features
 {
     public class GetBoardMembersTests
     {
-        ServiceCollection _services;
+        ServiceCollection _serviceCollection;
 
         public GetBoardMembersTests()
         {
-            _services = new ServiceCollection();
+            _serviceCollection = new ServiceCollection();
         }
 
         [Fact]
-        public async Task ShouldGetBoardMembers()
+        public async Task ShouldGetOnBoardMember()
         {
             var expectedResult = 1;
 
-            var context = new CoopDbContextBuilder()
-                .UseInMemoryDatabase()
-                .Build();
+            var container = _serviceCollection
+                .AddInMemoryDbContext()
+                .AddSingleton<IRequestHandler<Request, Response>, Handler>()
+                .BuildServiceProvider();
 
-            context.BoardMembers.Add(new ("President", "Quinntyne", "Brown", Guid.NewGuid()));
+            var context = container.GetService<ICoopDbContext>();
 
-            context.SaveChanges();
+            context.BoardMembers.Add(new("President", "Quinntyne", "Brown", Guid.NewGuid()));
 
-            _services.AddSingleton<ICoopDbContext>(context);
-            _services.AddSingleton<IRequestHandler<Request, Response>, Handler>();
-            
-            var container = _services.BuildServiceProvider();
+            await context.SaveChangesAsync(default);
 
             var sut = container.GetRequiredService<IRequestHandler<Request, Response>>();
 
             var result = await sut.Handle(new Request { }, default);
 
             Assert.Equal(expectedResult, result.BoardMembers.Count);
+        }
 
+        [Fact]
+        public async Task ShouldGetZeroBoardMembers()
+        {
+            var expectedResult = 0;
+
+            var container = _serviceCollection
+                .AddInMemoryDbContext()
+                .AddSingleton<IRequestHandler<Request, Response>, Handler>()
+                .BuildServiceProvider();
+
+            var sut = container.GetRequiredService<IRequestHandler<Request, Response>>();
+
+            var result = await sut.Handle(new Request { }, default);
+
+            Assert.Equal(expectedResult, result.BoardMembers.Count);
         }
     }
 }
